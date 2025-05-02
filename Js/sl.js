7c5271bb3eca818f2fc8141ccec47fcaabeda415
VinCat01 <Vindov.cat@gmail.com>
2025-05-02 20:51:55 +0300

    вторая часть страницы с описанием

--- Js/sl.js
+++ Js/sl.js
@@ -1,7 +1,5 @@
 const marqueeElement = document.querySelector('.marquee');
 
-// Настройка скорости (меньше значение — быстрее движение)
 let speedFactor = 10;
 
-// Применяем длительность анимации
 marqueeElement.style.animationDuration = `${speedFactor}s`;
